6e4e203fbcec991c1cf80da854ee89f80e2a6559
vworn <jasonkmathewpersonal@gmail.com>
2025-02-06 19:33:52 -0500

    Task 1 - Customer Profile.

---
+++ cc_5.js
@@ -0,0 +1,10 @@
+// Task 1: Object Properties - Customer Profile
+let customer = {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"
+};
+
+console.log(`Customer Name: ${customer.name}`);
+console.log(`Customer Age: ${customer.age}`);
+console.log(`Customer Email: ${customer.email}`);
